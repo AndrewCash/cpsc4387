5e2d60fb6c94bbdca9d74f3a3fb64bdeb2791cca
Andrew Cash <andrew@csh.casa>
2021-09-23 00:10:31 -0500

    customize fields in example

--- project1/cloud-run-template/app.py
+++ project1/cloud-run-template/app.py
@@ -7,13 +7,13 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def test_page():
-    project = "name_of_your_gcp_project"
+    project = "cpsc-4387-project-1"
     if request.method == 'POST':
         action = request.form['action']
-        pubsub_topic = "build_server_or_whatever_you_want"
+        pubsub_topic = "pubbly_subbly"
         publisher = pubsub_v1.PublisherClient()
         topic_path = publisher.topic_path(project, pubsub_topic)
-        future = publisher.publish(topic_path, data=b'Some text noone will read', action=action)
+        future = publisher.publish(topic_path, data=b'I feel vigorous', action=action)
         print(future.result())
     page_template = 'base_template.html'
     return render_template(page_template)
